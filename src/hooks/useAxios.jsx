b81f371353a730fbc7dceb11f416a08a99505401
tamanna-hash <tamannaemails@gmail.com>
2026-01-04 19:20:29 +0600

    change the baseURL

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import React from 'react';
 const axiosInstance = axios.create({
-    // baseURL:'https://travel-ease-server-nine.vercel.app/'
-    baseURL:'http://localhost:3000/'
+    baseURL:'https://travel-ease-server-nine.vercel.app/'
+    // baseURL:'http://localhost:3000/'
 })
 const useAxios = () => {
     return axiosInstance;
